313a32c065861d5796631c27144c904fc9815bcd
timothee afonso <tim.afonso22@gmail.com>
2022-11-17 09:46:29 +0100

    update

--- second_model.py
+++ second_model.py
@@ -1,18 +1,20 @@
-from pickle import TRUE
 from pycsp3 import *
 
 n, e = data
 CB = 7
 
 x = VarArray(size=n, dom=range(1,n+1))
-y = VarArray(size=len(e), dom=range(1,CB+1))
+#y = VarArray(size=len(e), dom=range(1,CB+1))
+z = []
 
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if (abs(i-j) <=CB or abs(i-j)>=n-CB) and i!=j:
+            z.append((i,j))
+            z.append((j,i))
 satisfy(
     AllDifferent(x),
-    CB in y ==TRUE,
-    [(interval <= CB) | (interval>=n-CB) for interval in y],
-    [abs(x[edge[0]-1] - x[edge[1]-1]) == y[idx] for idx,edge in enumerate(e)]
-
+    [(x[i-1], x[j-1]) in z for i,j in e]
 )
 
 #solve()
