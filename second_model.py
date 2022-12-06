3b9dba56348c4464c6e82b6bc32c40a6b3b5dbdb
gillie04th <theophile.gillier@gmail.com>
2022-12-06 12:02:03 +0100

    second model opti qui fonctionne

--- second_model.py
+++ second_model.py
@@ -1,33 +1,38 @@
 from pycsp3 import *
 import time
 
+start = time.time()
 n, e = data
-interval=[0,n]
 x = VarArray(size=n, dom=range(1,n+1))
 
+intervale = [1,n]
+
 def find(intervale):
-    start = time.time()
-    k=int((intervale[1]-1+intervale[0])/2)
-    res = search(k)
-    print("k:" + str(k) + ", res:" + str(res) + ", interval:" + str(intervale) + ", time:" + str(time.time() - start))
-    if(intervale[1]-intervale[0] <= 2):
-        print("Lower bound : " + str(k))
+    z = []
+    k=int((intervale[1]+intervale[0])/2)
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            #if([i-1,j-1] in e or [j-1,i-1] in e):
+                if (abs(i-j) <=k or abs(i-j)>=n-k) and i!=j:
+                    z.append((i,j))
+    print("e:" + str(len(e)) + " z:" + str(len(z)) + " x:" + str(len(x)))
+    satisfy(
+        AllDifferent(x),
+        [(x[i-1], x[j-1]) in z for i,j in e]
+    )
+    res = solve(solver=ACE, options="-t=" + str(n) + "s", sols=1)
+    unpost()
+    print("k:" + str(k) + ", res:" + str(res) + ", interval:" + str(intervale))
+    if(intervale[1]-intervale[0] <= 1):
+        if(res is SAT):
+            print("Lower bound is " + str(k))
+        else:
+            print("Lower bound is " + str(intervale[1]))
+        print("time:" + str(time.time() - start))
     elif(res is SAT):
         find([intervale[0], k])
     elif(res is UNSAT):
         find([k,intervale[1]])
 
-def search(k):
-    z = []
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            if (abs(i-j) <=k or abs(i-j)>=n-k) and i!=j:
-                z.append((i,j))
-    print("z:" + str(z.__sizeof__()) + " x:" + str(x.__sizeof__()))
-    satisfy(
-        AllDifferent(x),
-        [(x[i-1], x[j-1]) in z for i,j in e]
-    )
-    return solve(solver=ACE, options="-t=" + str(n) + "s")
 
 find([1,n])
--- solve.py
+++ solve.py
@@ -6,21 +6,23 @@ from os.path import isfile, join
 ### 
 
 files = [
-    #"ash85",
-    #"bcspwr01",
-    #"bcspwr02",
-    #"curtis54",
-    #"ibm32",        # 9 pour le min en model2
-    #"pores_1",
+    "ash85",
+    "bcspwr01",
+    "bcspwr02",
+    "curtis54",
+    "ibm32",
+    "pores_1",
     "steam3",
-    #"will57",
-    #"bcsstk01",
-    #"impcol_b"
+    "will57",
+    "bcsstk01",
+    "impcol_b"
 ]
 #files = [f for f in listdir("data/") if isfile(join("data/", f))]
 if(sys.argv[1] != "third"):
     for file in files:
+        print(file + ":")
         os.system('python3 ' + sys.argv[1] + '_model.py -data=data/' + file + '.mtx.rnd -dataparser=parser.py -solver=[ace,v,args="-t=' + sys.argv[2] + 's"] -ev')
 else:
     for file in files:
+        print(file + ":")
         os.system('python3 ' + sys.argv[1] + '_model.py -data=data/' + file + '.mtx.rnd -dataparser=parser.py')
